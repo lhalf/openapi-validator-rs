e6d52fd0aeb62094ec9dda1adaf993146a56b9e3
Laurence Halfpenny <laurence.halfpenny@paconsulting.com>
2023-03-10 15:33:43 +0000

    add validator for get method being in openapi spec

--- src/validator.rs
+++ src/validator.rs
@@ -10,7 +10,7 @@ impl Validator {
 
     //take &self rather than self otherwise Validator is consumed by validate_request (dropped)
     fn validate_request(&self, request: Request) -> Result<Request, ()> {
-        self.validate_path(request)
+        self.validate_path(request).and_then(|request| self.validate_method(request))
     }
 
     fn validate_path(&self, request: Request) -> Result<Request, ()> {
@@ -19,16 +19,35 @@ impl Validator {
         }
         Err(())
     }
+
+    fn validate_method(&self, request: Request) -> Result<Request, ()> {
+        match request.method() {
+            "get" => self.validate_get(request),
+            _ => Err(())
+        }
+    }
+
+    fn validate_get(&self, request: Request) -> Result<Request, ()> {
+        if self.api.paths.paths[request.path.as_str()].as_item().unwrap().get.is_some() {
+            return Ok(request);
+        }
+        Err(())
+    }
 }
 
 #[derive(Debug, PartialEq)]
 struct Request {
-    path: String
+    path: String,
+    method: String
 }
 
 impl Request {
     fn path(&self) -> &str {
         &self.path
+    }
+
+    fn method(&self) -> &str {
+        &self.method
     }
 }
 
@@ -50,14 +69,21 @@ mod test {
     #[test]
     fn validator_can_work_a_request_with_valid_path() {
         let validator = make_validator();
-        let request = Request{path: "/ping".to_string()};
+        let request = Request{path: "/ping".to_string(), method: "get".to_string()};
         assert!(validator.validate_request(request).is_ok());
     }
 
     #[test]
     fn validator_can_reject_a_request_with_invalid_path() {
         let validator = make_validator();
-        let request = Request{path: "/not/ping".to_string()};
+        let request = Request{path: "/not/ping".to_string(), method: "get".to_string()};
+        assert_eq!(Err(()), validator.validate_request(request));
+    }
+
+    #[test]
+    fn validator_can_reject_a_request_with_invalid_method() {
+        let validator = make_validator();
+        let request = Request{path: "/ping".to_string(), method: "put".to_string()};
         assert_eq!(Err(()), validator.validate_request(request));
     }
 }
