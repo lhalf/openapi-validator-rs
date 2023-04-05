dfacacaf7d7578fa7b6705574a868444d9dbd185
Laurence Halfpenny <laurence.halfpenny@paconsulting.com>
2023-04-05 16:03:09 +0100

    validate body if requestBody required is true

--- src/validator.rs
+++ src/validator.rs
@@ -11,7 +11,8 @@ impl Validator {
     //take &self rather than self otherwise Validator is consumed by validate_request (dropped)
     fn validate_request(&self, request: Request) -> Result<Request, ()> {
         self.validate_path(request.path())?
-            .validate_method(request.method())?;
+            .validate_operation(request.operation())?
+            .validate_body(request.body())?;
         Ok(request)
     }
 
@@ -28,8 +29,8 @@ struct ValidatedPath<'path> {
 }
 
 impl<'path>  ValidatedPath<'path>  {
-    fn validate_method(&self, method: &str) -> Result<(), ()> {
-        match method {
+    fn validate_operation(&self, operation: &str) -> Result<ValidatedOperation, ()> {
+        match operation {
             "get" => self.validate_get(),
             "put" => self.validate_put(),
             "delete" => self.validate_delete(),
@@ -38,40 +39,55 @@ impl<'path>  ValidatedPath<'path>  {
         }
     }
 
-    fn validate_get(&self) -> Result<(), ()> {
-        if self.path.get.is_some() {
-            return Ok(());
+    fn validate_get(&self) -> Result<ValidatedOperation, ()> {
+        if let Some(operation) = self.path.get.as_ref() {
+            return Ok(ValidatedOperation{operation});
         }
         Err(())
     }
 
-    fn validate_put(&self) -> Result<(), ()> {
-        if self.path.put.is_some() {
-            return Ok(());
+    fn validate_put(&self) -> Result<ValidatedOperation, ()> {
+        if let Some(operation) = self.path.put.as_ref() {
+            return Ok(ValidatedOperation{operation});
         }
         Err(())
     }
 
-    fn validate_post(&self) -> Result<(), ()> {
-        if self.path.post.is_some() {
-            return Ok(());
+    fn validate_post(&self) -> Result<ValidatedOperation, ()> {
+        if let Some(operation) = self.path.post.as_ref() {
+            return Ok(ValidatedOperation{operation});
         }
         Err(())
     }
 
-    fn validate_delete(&self) -> Result<(), ()> {
-        if self.path.delete.is_some() {
-            return Ok(());
+    fn validate_delete(&self) -> Result<ValidatedOperation, ()> {
+        if let Some(operation) = self.path.delete.as_ref() {
+            return Ok(ValidatedOperation{operation});
         }
         Err(())
     }
 }
 
+struct ValidatedOperation<'operation> {
+    operation: &'operation openapiv3::Operation
+}
+
+impl<'operation>  ValidatedOperation<'operation> {
+    fn validate_body(&self, body: &[u8]) -> Result<(), ()> {
+        if let Some(spec_body) = self.operation.request_body.as_ref().and_then(openapiv3::ReferenceOr::as_item) {
+            if spec_body.required && body.is_empty() {
+                return Err(());
+            }
+        }
+        Ok(())
+    }
+}
 
 #[derive(Debug, PartialEq)]
 struct Request {
     path: String,
-    method: String
+    operation: String,
+    body: Vec<u8>
 }
 
 impl Request {
@@ -79,8 +95,12 @@ impl Request {
         &self.path
     }
 
-    fn method(&self) -> &str {
-        &self.method
+    fn operation(&self) -> &str {
+        &self.operation
+    }
+
+    fn body(&self) -> &[u8] {
+        &self.body
     }
 }
 
@@ -103,7 +123,8 @@ mod test {
     fn validator_can_work_a_request_with_valid_path() {
         let validator = make_validator();
         let request = Request{path: "/ping".to_string(),
-                              method: "get".to_string()};
+                              operation: "get".to_string(),
+                              body: vec![]};
         assert!(validator.validate_request(request).is_ok());
     }
 
@@ -111,31 +132,44 @@ mod test {
     fn validator_can_reject_a_request_with_invalid_path() {
         let validator = make_validator();
         let request = Request{path: "/not/ping".to_string(),
-                              method: "get".to_string()};
+                              operation: "get".to_string(),
+                              body: vec![]};
         assert_eq!(Err(()), validator.validate_request(request));
     }
 
     #[test]
-    fn validator_can_work_with_a_request_with_put_method() {
+    fn validator_can_work_with_a_request_with_put_operation() {
         let validator = make_validator();
-        let request = Request{path: "/multiple/allowed/methods".to_string(),
-                              method: "put".to_string()};
+        let request = Request{path: "/multiple/allowed/operations".to_string(),
+                              operation: "put".to_string(),
+                              body: vec![]};
         assert!(validator.validate_request(request).is_ok());
     }
 
     #[test]
-    fn validator_can_work_with_a_request_with_post_method() {
+    fn validator_can_work_with_a_request_with_post_operation() {
         let validator = make_validator();
-        let request = Request{path: "/multiple/allowed/methods".to_string(),
-                              method: "post".to_string()};
+        let request = Request{path: "/multiple/allowed/operations".to_string(),
+                              operation: "post".to_string(),
+                              body: vec![]};
         assert!(validator.validate_request(request).is_ok());
     }
 
     #[test]
-    fn validator_can_work_with_a_request_with_delete_method() {
+    fn validator_can_work_with_a_request_with_delete_operation() {
         let validator = make_validator();
-        let request = Request{path: "/multiple/allowed/methods".to_string(),
-                              method: "delete".to_string()};
+        let request = Request{path: "/multiple/allowed/operations".to_string(),
+                              operation: "delete".to_string(),
+                              body: vec![]};
         assert!(validator.validate_request(request).is_ok());
+    }
+
+    #[test]
+    fn validator_can_reject_a_request_with_no_body_if_required() {
+        let validator = make_validator();
+        let request = Request{path: "/required/body".to_string(),
+                              operation: "post".to_string(),
+                              body: vec![]};
+        assert_eq!(Err(()), validator.validate_request(request));
     }
 }
