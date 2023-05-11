3958ddaaef720d0ee3e448c24fd97b28573b03a3
Laurence Halfpenny <laurence.halfpenny@paconsulting.com>
2023-05-11 09:52:13 +0100

    implement jsonschema for integer types

--- src/jsonschema.rs
+++ src/jsonschema.rs
@@ -15,6 +15,9 @@ impl JSONSchema for openapiv3::Schema {
             openapiv3::SchemaKind::Type(Type::Number(number_schema)) => {
                 number_schema.to_json_schema()
             }
+            openapiv3::SchemaKind::Type(Type::Integer(integer_schema)) => {
+                integer_schema.to_json_schema()
+            }
             openapiv3::SchemaKind::Type(Type::Array(array_schema)) => array_schema.to_json_schema(),
             _ => todo!(),
         }
@@ -61,6 +64,35 @@ impl JSONSchema for openapiv3::NumberType {
     fn to_json_schema(&self) -> serde_json::Value {
         let mut json = serde_json::Map::new();
         json.insert("type".to_string(), serde_json::Value::from("number"));
+        if let Some(minimum) = self.minimum {
+            json.insert("minimum".to_string(), minimum.into());
+        }
+        if let Some(maximum) = self.maximum {
+            json.insert("maximum".to_string(), maximum.into());
+        }
+        if self.exclusive_minimum {
+            json.insert(
+                "exclusiveMinimum".to_string(),
+                self.exclusive_minimum.into(),
+            );
+        }
+        if self.exclusive_maximum {
+            json.insert(
+                "exclusiveMaximum".to_string(),
+                self.exclusive_minimum.into(),
+            );
+        }
+        if let Some(multiple_of) = self.multiple_of {
+            json.insert("multipleOf".to_string(), multiple_of.into());
+        }
+        json.into()
+    }
+}
+
+impl JSONSchema for openapiv3::IntegerType {
+    fn to_json_schema(&self) -> serde_json::Value {
+        let mut json = serde_json::Map::new();
+        json.insert("type".to_string(), serde_json::Value::from("integer"));
         if let Some(minimum) = self.minimum {
             json.insert("minimum".to_string(), minimum.into());
         }
@@ -321,6 +353,112 @@ mod test_number {
             }
             .to_json_schema(),
             json!({"type": "number", "multipleOf": 1.1})
+        )
+    }
+}
+
+#[cfg(test)]
+mod test_integer {
+    use super::*;
+    use openapiv3::IntegerType;
+
+    #[test]
+    fn basic() {
+        assert_eq!(
+            openapiv3::Schema {
+                schema_data: Default::default(),
+                schema_kind: openapiv3::SchemaKind::Type(Type::Integer(IntegerType {
+                    format: Default::default(),
+                    multiple_of: None,
+                    exclusive_minimum: false,
+                    exclusive_maximum: false,
+                    minimum: None,
+                    maximum: None,
+                    enumeration: vec![],
+                }))
+            }
+            .to_json_schema(),
+            json!({"type": "integer"})
+        )
+    }
+
+    #[test]
+    fn minimum() {
+        assert_eq!(
+            openapiv3::Schema {
+                schema_data: Default::default(),
+                schema_kind: openapiv3::SchemaKind::Type(Type::Integer(IntegerType {
+                    format: Default::default(),
+                    multiple_of: None,
+                    exclusive_minimum: false,
+                    exclusive_maximum: false,
+                    minimum: Some(2),
+                    maximum: None,
+                    enumeration: vec![],
+                }))
+            }
+            .to_json_schema(),
+            json!({"type": "integer", "minimum": 2})
+        )
+    }
+
+    #[test]
+    fn minimum_and_maximum() {
+        assert_eq!(
+            openapiv3::Schema {
+                schema_data: Default::default(),
+                schema_kind: openapiv3::SchemaKind::Type(Type::Integer(IntegerType {
+                    format: Default::default(),
+                    multiple_of: None,
+                    exclusive_minimum: false,
+                    exclusive_maximum: false,
+                    minimum: Some(2),
+                    maximum: Some(5),
+                    enumeration: vec![],
+                }))
+            }
+            .to_json_schema(),
+            json!({"type": "integer", "minimum": 2, "maximum": 5})
+        )
+    }
+
+    #[test]
+    fn exclusive_minimum_and_maximum() {
+        assert_eq!(
+            openapiv3::Schema {
+                schema_data: Default::default(),
+                schema_kind: openapiv3::SchemaKind::Type(Type::Integer(IntegerType {
+                    format: Default::default(),
+                    multiple_of: None,
+                    exclusive_minimum: true,
+                    exclusive_maximum: true,
+                    minimum: Some(2),
+                    maximum: Some(5),
+                    enumeration: vec![],
+                }))
+            }
+            .to_json_schema(),
+            json!({"type": "integer", "minimum": 2, "maximum": 5, "exclusiveMinimum": true, "exclusiveMaximum": true})
+        )
+    }
+
+    #[test]
+    fn multiple_of() {
+        assert_eq!(
+            openapiv3::Schema {
+                schema_data: Default::default(),
+                schema_kind: openapiv3::SchemaKind::Type(Type::Integer(IntegerType {
+                    format: Default::default(),
+                    multiple_of: Some(10),
+                    exclusive_minimum: false,
+                    exclusive_maximum: false,
+                    minimum: None,
+                    maximum: None,
+                    enumeration: vec![],
+                }))
+            }
+            .to_json_schema(),
+            json!({"type": "integer", "multipleOf": 10})
         )
     }
 }
