965e11505f3a6631c8708a9be6a210c1d61eeccb
Laurence Halfpenny <laurence.halfpenny@halfpenny.net>
2023-08-08 20:29:57 +0100

    update item_or_fetch.rs impl to use a macro

--- src/item_or_fetch.rs
+++ src/item_or_fetch.rs
@@ -4,53 +4,42 @@ pub trait ItemOrFetch<T> {
     fn item_or_fetch<'api>(&'api self, components: &'api Option<openapiv3::Components>) -> &T;
 }
 
-impl ItemOrFetch<openapiv3::Schema> for openapiv3::ReferenceOr<openapiv3::Schema> {
-    fn item_or_fetch<'api>(
-        &'api self,
-        components: &'api Option<openapiv3::Components>,
-    ) -> &openapiv3::Schema {
-        match self {
-            Self::Item(item) => item,
-            Self::Reference { reference } => components
-                .as_ref()
-                .unwrap()
-                .schemas
-                .index(reference.trim_start_matches("#/components/schemas/"))
-                .item_or_fetch(components),
+macro_rules! item_or_fetch_impl {
+    ($item_ty:ty, $reference_ty:ty, $component_field:ident, $component_path:expr) => {
+        impl ItemOrFetch<$item_ty> for $reference_ty {
+            fn item_or_fetch<'api>(
+                &'api self,
+                components: &'api Option<openapiv3::Components>,
+            ) -> &$item_ty {
+                match self {
+                    Self::Item(item) => item,
+                    Self::Reference { reference } => components
+                        .as_ref()
+                        .unwrap()
+                        .$component_field
+                        .index(reference.trim_start_matches($component_path))
+                        .item_or_fetch(components),
+                }
+            }
         }
-    }
+    };
 }
 
-impl ItemOrFetch<openapiv3::Parameter> for openapiv3::ReferenceOr<openapiv3::Parameter> {
-    fn item_or_fetch<'api>(
-        &'api self,
-        components: &'api Option<openapiv3::Components>,
-    ) -> &openapiv3::Parameter {
-        match self {
-            Self::Item(item) => item,
-            Self::Reference { reference } => components
-                .as_ref()
-                .unwrap()
-                .parameters
-                .index(reference.trim_start_matches("#/components/parameters/"))
-                .item_or_fetch(components),
-        }
-    }
-}
-
-impl ItemOrFetch<openapiv3::RequestBody> for openapiv3::ReferenceOr<openapiv3::RequestBody> {
-    fn item_or_fetch<'api>(
-        &'api self,
-        components: &'api Option<openapiv3::Components>,
-    ) -> &openapiv3::RequestBody {
-        match self {
-            Self::Item(item) => item,
-            Self::Reference { reference } => components
-                .as_ref()
-                .unwrap()
-                .request_bodies
-                .index(reference.trim_start_matches("#/components/requestBodies/"))
-                .item_or_fetch(components),
-        }
-    }
-}
+item_or_fetch_impl!(
+    openapiv3::Schema,
+    openapiv3::ReferenceOr<openapiv3::Schema>,
+    schemas,
+    "#/components/schemas/"
+);
+item_or_fetch_impl!(
+    openapiv3::Parameter,
+    openapiv3::ReferenceOr<openapiv3::Parameter>,
+    parameters,
+    "#/components/parameters/"
+);
+item_or_fetch_impl!(
+    openapiv3::RequestBody,
+    openapiv3::ReferenceOr<openapiv3::RequestBody>,
+    request_bodies,
+    "#/components/requestBodies/"
+);
